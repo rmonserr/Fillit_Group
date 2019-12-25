15a5a826d173472148ff636c386c10e094b8357e
Ruthie Monserrate <rmonserr@ga-x2.21-school.ru>
2019-12-25 17:55:12 +0300

    BuffSize error

---
+++ ForGit/Fillit_final/src/output.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmonserr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/17 17:30:52 by rmonserr          #+#    #+#             */
+/*   Updated: 2019/12/17 17:30:54 by rmonserr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+void	ft_out_map(char **result, int size)
+{
+	int	x;
+	int y;
+
+	y = 0;
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			ft_putchar(result[y][x]);
+			x++;
+		}
+		ft_putchar('\n');
+		y++;
+	}
+	ft_putchar('\n');
+	ft_memdel((void **)result);
+}
